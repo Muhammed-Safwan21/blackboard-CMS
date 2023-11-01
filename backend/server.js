c06e1fe870568a4dc1cf8a973a6d94f2958517c7
Muhammed Safwan K <muhammedsafwank00@gmail.com>
2023-11-01 09:11:10 +0530

    Updated Logo and Server.js file

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,23 @@ app.use('/api/upload',uploadRoutes)
 const __dirname =path.resolve()  // set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
+
+
 app.use(notFound);
 app.use(errorHandler);
 
